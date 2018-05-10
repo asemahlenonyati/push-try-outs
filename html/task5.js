fe1b9b339cb09de9fbdd11a49d0cc6743105e1d4
Asemahle_No <asemahlenonyati@gmail.com>
2018-05-10 14:16:04 +0200

    save my work

---
+++ html/task5.js
@@ -0,0 +1,16 @@
+(function(){
+	var count =1;
+	document.getElementById("button").addEventListener('click', function(){
+     
+     count++
+     if (count%2===0) {
+     	document.getElementById("box").style.backgroundColor = "green"
+     	document.getElementById("button").innerHTML = "make red"
+     }
+     else {
+     	document.getElementById("box").style.backgroundColor = "red"
+     	document.getElementById("button").innerHTML = "make green"
+     }
+
+})
+})()
